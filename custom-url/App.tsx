b8e70fc20586bcbe46638adf4ec408373187bc58
Yasuo Higa <higayasuo@gmail.com>
2023-08-25 15:54:20 +0900

    Add parsing URL example

--- custom-url/App.tsx
+++ custom-url/App.tsx
@@ -4,10 +4,14 @@ import * as Linking from 'expo-linking';
 
 export default function App() {
   const url = Linking.useURL();
+  const { hostname, path, queryParams } = Linking.parse(url + '');
 
   return (
     <View style={tw('flex-1 items-center justify-center')}>
-      <Text style={tw('text-3xl')}>HAIP app URL:{url}</Text>
+      <Text style={tw('text-3xl')}>
+        HAIP app hostname:{hostname} path:{path} queryParams:
+        {JSON.stringify(queryParams, undefined, 2)}
+      </Text>
     </View>
   );
 }
